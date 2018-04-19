581b5b1fff7804aa0b946ce3fddcf6d1767fa6cb
g071191 <JLGrindrod@hotmail.com>
2018-04-19 11:56:40 +0100

    reverses a string

--- reverse.py
+++ reverse.py
@@ -2,6 +2,8 @@ def reverse(text):
     word = ""
     l = len(text) - 1
     while l >= 0:
-        word = word + text[l]
+        word += text[l]
         l -= 1
     return word
+
+print(reverse("Hello"))
