946d95fff95f4cc383e02ecd23b796b2b39d7057
g071191 <JLGrindrod@hotmail.com>
2018-04-19 11:43:23 +0100

    remove duplicate number(s) from a list

---
+++ remove_duplicates.py
@@ -0,0 +1,10 @@
+def remove_duplicates(n):
+    new_list = []
+
+    for i in n:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+
+print (remove_duplicates([1, 1, 2, 2]))
