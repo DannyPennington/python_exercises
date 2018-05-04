5779dbf8431d78c5c18096c504ede4bff4ea6f3a
jordan-grindrod <JLGrindrod@hotmail.com>
2018-05-04 18:03:24 +0100

    Starting to work with Databases

---
+++ database.py
@@ -0,0 +1,23 @@
+import pymysql
+import sys
+
+db = pymysql.connect("localhost", "root", "city2939", "Practice")
+c = db.cursor()
+
+while 1:
+    id = int(input("Enter ID number: "))
+    name = str(input("Enter name: "))
+    score = int(input("Enter Score: "))
+    query=f"insert into test_results values({id},'{name}',{score})"
+    c.execute(query)
+    db.commit()
+    print("")
+    repeat_entry = str(input("Would you like to enter another score? (Y/N): "))
+    if repeat_entry == "Y" or repeat_entry == "y":
+        1
+    elif repeat_entry == "N" or repeat_entry == "n":
+        sys.exit()
+    else:
+        print("")
+        print("You didn't enter 'Y' or 'N'!")
+    print("")
