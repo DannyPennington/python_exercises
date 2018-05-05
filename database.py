9f5f143afa98f911cefb2f26df2a8d20f6385da3
jordan-grindrod <JLGrindrod@hotmail.com>
2018-05-05 10:34:59 +0100

    Starting to work with Databases

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import pymysql
 import sys
 
-db = pymysql.connect("localhost", "root", "city2939", "Practice")
+db = pymysql.connect("localhost", "root", "****", "Practice")
 c = db.cursor()
 
 while 1:
